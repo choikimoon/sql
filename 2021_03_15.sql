4ec95490519fec731e6e2c5cca895de8c533f9db
DESKTOP-5M0B913\crm95 <crm950828@gmail.com>
2021-03-15 19:57:33 +0900

    과제 완료

--- 2021_03_15.sql
+++ 2021_03_15.sql
@@ -353,6 +353,10 @@ WHERE job = 'SALESMAN' OR empno LIKE ('78%'); -- empno는 숫자를 사용하는
 --논리연산 (AND, OR 실습 where13)
 --emp 테이블에서 job이 SALESMAN이거나 사원번호가 78로 시작하는 직원의 정보를 다음과 같이 조회 하세요 (LIKE 연산자 사용 금지)
 
+SELECT *
+FROM emp
+WHERE job = 'SALESMAN' OR empno BETWEEN 7800 AND 7899;
+
 
 
 
